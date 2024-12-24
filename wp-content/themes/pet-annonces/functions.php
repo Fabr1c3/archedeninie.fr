5a04b477b0542b5e58f584436b728be09d2ee50e
Fabrice <fab.dhuicque@gmail.com>
2024-12-24 18:39:13 +0100

    hotfix

--- wp-content/themes/pet-annonces/functions.php
+++ wp-content/themes/pet-annonces/functions.php
@@ -248,7 +248,7 @@ add_filter('wp_sitemaps_taxonomies', 'arche_disable_sitemap_taxonomy');
 
 function getAnimalAge($age = null, $unite = null, $birthDate = null) {
     // Si aucune date de naissance n'est fournie
-    if (empty($birthDate)) {
+    if ($birthDate === null) {
         // Traiter en fonction de l'âge et de l'unité
         if (empty($age)) {
             return '';
