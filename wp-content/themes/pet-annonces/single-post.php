23f5acbba9a265970bb7780c622c52e8fe432002
Fabrice <fab.dhuicque@gmail.com>
2025-01-06 11:38:21 +0100

    single post page enhancement

--- wp-content/themes/pet-annonces/single-post.php
+++ wp-content/themes/pet-annonces/single-post.php
@@ -15,10 +15,10 @@ endif;
             <h1 class="mb-3 text-center"><?= get_the_title(); ?></h1>
             <p class="text-muted text-center mb-2 mb-lg-5 m-auto">Publié le <?= get_the_date(); ?></p>
             <div class="row">
-                <div class="col-12 col-lg-6 m-auto p-3">
+                <div class="col-12 m-auto p-3">
                     <img src="<?= $image; ?>" class="rounded img-actus w-100" alt="<?= get_the_title(); ?>">
                 </div>
-                <div class="post-content col-lg-6 col-12 text-justify m-auto">
+                <div class="post-content col-lg-10 col-12 text-justify m-auto">
                     <?= apply_filters('the_content', get_post_field('post_content')); ?>
                 </div>
             </div>
